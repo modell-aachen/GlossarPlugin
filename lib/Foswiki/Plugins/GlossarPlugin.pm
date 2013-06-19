91c49b69755ece13928a41489edd9486f86abed9
Stephan Osthold <osthold@modell-aachen.de>
2013-06-19 14:56:02 +0200

    Release 0.9.7

--- lib/Foswiki/Plugins/GlossarPlugin.pm
+++ lib/Foswiki/Plugins/GlossarPlugin.pm
@@ -29,7 +29,7 @@ use Foswiki::Plugins ();  # For the API version
 
 our $VERSION = '$Rev: 8536 $';
 
-our $RELEASE = '0.9.6';
+our $RELEASE = '0.9.7';
 
 our $SHORTDESCRIPTION = 'Creates a pop-up glossar for your wiki.';
 
