ca5c759f8f5d19aae1bac6a0a54391edefb3008c
Maik Glatki <glatki@modell-aachen.de>
2015-09-16 16:37:20 +0200

    Add a missing french translation file.
    Prevents a rare script error occuring under specific conditions.
    
    Ticket: None
    Release-Note(user,fix){
    Add a missing french translation file.
    [...]
    Prevents a rare script error occuring under specific conditions.
    }

---
+++ pub/System/GlossarPlugin/jquery.thesaurus_fr_src.js
@@ -0,0 +1,7 @@
+// GlossarPlugin: French language file
+GlossarLang = {
+    ambiguous: '(plusieurs significations)',
+    disambiguate: 'Il y existe plusieurs définitions pour ce terme:',
+    btn_edit_label: 'Modifier'
+};
+
