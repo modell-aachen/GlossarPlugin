584e72a3c8852f22d511a7b63bba2d464f592e21
Stephan Osthold <osthold@modell-aachen.de>
2015-12-07 17:25:35 +0100

    Cleanup wrong chinese translation
    Ticket: none
    Release-Note: none

--- pub/System/GlossarPlugin/jquery.thesaurus_cn_src.js
+++
@@ -1,7 +0,0 @@
-// GlossarPlugin: Chinese language file
-GlossarLang = {
-    ambiguous: '(multiple meanings)',
-    disambiguate: 'There are several definitions for this term:',
-    btn_edit_label: 'Edit'
-};
-
