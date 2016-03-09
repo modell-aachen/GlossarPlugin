7caec524596668854c8bb22c3e7471acdb879420
Daniel Supplieth <supplieth@modell-aachen.de>
2016-03-09 11:48:55 +0100

    Delete speech bubble style, it was only shown in chrome and not so fancy.
    Ticket: #11872
    Release-Note: none

--- pub/System/GlossarPlugin/jquery.thesaurus_src.js
+++ pub/System/GlossarPlugin/jquery.thesaurus_src.js
@@ -18,11 +18,11 @@ var lang = GlossarLang,
     TPL_TAG_CLOSE = '~~',
     UNAPPROPRIATE_TAGS = {'SCRIPT':1, 'BASE':1, 'LINK':1, 'META':1, 'STYLE':1, 'TITLE':1, 'APPLET':1, 'OBJECT':1, 'TEXTAREA':1, 'FORM':1, 'INPUT':1, 'DFN':1, 'svg':1, 'SVG':1},
     DEFAULTCSS_TPL =
-        'div.thesaurus { font-size: 12px; font-family: Arial; position: absolute; width: 720px; z-index: auto; -moz-box-shadow: 5px 5px 5px #444; -webkit-box-shadow: 5px 5px 5px #444; }' +
-        'div.thesaurus .thesaurus-header { padding: 5px;  background-color: #3C5F87; -moz-border-radius: 5px 5px 0 0; -webkit-border-radius: 5px 5px 0 0; }' +
+        'div.thesaurus { font-size: 12px; font-family: Arial; position: absolute; width: 720px; z-index: auto;}' +
+        'div.thesaurus .thesaurus-header { padding: 5px;  background-color: #3C5F87;}' +
         'div.thesaurus .thesaurus-header a.term { color: white; font-weight: bold; }' +
         'div.thesaurus .thesaurus-header .term_editbtn { float: right; }' +
-        'div.thesaurus .thesaurus-body { padding: 5px;  border: 1px solid #3C5F87; background-color: #fff; -moz-border-radius: 0 0 0 5px; -webkit-border-radius: 0 0 0 5px; }' +
+        'div.thesaurus .thesaurus-body { padding: 5px;  border: 1px solid #3C5F87; background-color: #fff;}' +
         'div.thesaurus .thesaurus-addendum { background-color: #ddd; padding-bottom: 0.5em; }' +
         'div.thesaurus .thesaurus-alts ul { margin: 0; }' +
         'dfn.thesaurus { text-decoration: none; font-style: inherit; border-bottom: 1px dashed black; cursor: pointer; }' +
